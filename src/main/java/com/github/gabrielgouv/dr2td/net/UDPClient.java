c6e5fdf32f4f0de89729e00b80c2b0a35a16bee2
gabrielgouv <joao.gabriel.gouveia2@gmail.com>
2021-12-24 19:56:29 -0300

    feat: add popup messages for port already in use and unexpected errors

--- src/main/java/com/github/gabrielgouv/dr2td/net/UDPClient.java
+++ src/main/java/com/github/gabrielgouv/dr2td/net/UDPClient.java
@@ -1,5 +1,7 @@
 package com.github.gabrielgouv.dr2td.net;
 
+import javax.swing.*;
+import java.net.BindException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.util.function.Consumer;
@@ -32,8 +34,13 @@ public class UDPClient {
                     onPacketReceived.accept(packet);
                 }
             }
+        } catch (BindException bindException) {
+            JOptionPane.showMessageDialog(null, String.format("Port %d is already in use by another application.\n" +
+                    "Change the port in the settings.cfg file or close applications that are using this port.", port));
+            System.exit(1);
         } catch (Exception e) {
-            e.printStackTrace();
+            JOptionPane.showMessageDialog(null, String.format("Unexpected error occurred:\n%s", e.getLocalizedMessage()));
+            System.exit(2);
         }
     }
 
