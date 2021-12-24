1ecf132cdbb15aa96735d50a517b530118cb9d91
gabrielgouv <joao.gabriel.gouveia2@gmail.com>
2021-12-24 19:55:24 -0300

    feat: set application look and feel to use system look and feel

--- src/main/java/com/github/gabrielgouv/dr2td/bootstrap/Bootstrap.java
+++ src/main/java/com/github/gabrielgouv/dr2td/bootstrap/Bootstrap.java
@@ -5,6 +5,7 @@ import com.github.gabrielgouv.dr2td.factory.TelemetryDataFactory;
 import com.github.gabrielgouv.dr2td.gui.Dashboard;
 import com.github.gabrielgouv.dr2td.net.UDPClient;
 
+import javax.swing.*;
 import java.net.DatagramPacket;
 
 public class Bootstrap {
@@ -14,9 +15,20 @@ public class Bootstrap {
     public static void main(String[] args) {
         // TODO: load config file
 
+        setupLookAndFeel();
+
         final var udpClient = new UDPClient(10001); // FIXME: get port from config file
         udpClient.onPacketReceived((Bootstrap::updateDashboard));
         udpClient.listen();
+    }
+
+    private static void setupLookAndFeel() {
+        try {
+            UIManager.setLookAndFeel(
+                    UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            System.err.println("Cannot apply look and feel");
+        }
     }
 
     private static void updateDashboard(DatagramPacket packet) {
